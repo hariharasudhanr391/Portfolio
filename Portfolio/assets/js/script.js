a3e81aa60721e6fc37c117a5e78c33867523af02
Harihara Sudhan R <68938791+hariharasudhanr391@users.noreply.github.com>
2024-07-02 10:11:24 +0530

    Add files via upload

---
+++ Portfolio/assets/js/script.js
@@ -0,0 +1,35 @@
+let navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach((item) => {
+    item.addEventListener('click',function(){
+        document.querySelector('nav ul li a.active').classList.remove('active')
+        item.classList.add('active')
+        document.querySelector('main > section.active').classList.remove('active')
+        document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active')
+    })
+    
+})
+
+
+document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function(){
+    document.querySelector('#sidebar').classList.toggle('open')
+})
+
+
+
+var options = {
+    strings: ['Front-End web Developer', 'UI/UX Designer', 'Web Designer', 'Graphic Designer'],
+    loop: true,
+    typeSpeed: 70,
+    backSpeed: 10
+  };
+
+new Typed('.field h2', options);
+
+
+
+for(let i=1; i<=15; i++){
+    let meteor = document.createElement('span');
+    meteor.classList = 'meteor'
+    document.querySelector('#home .meteor-shower').append(meteor)
+}
